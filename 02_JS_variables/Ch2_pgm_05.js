c728915120eaabd256589a4ca1effb0a8427da4c
2226j1554 <144467760+2226j1554@users.noreply.github.com>
2024-07-02 09:58:42 +0530

    Update Ch2_pgm_05.js

--- 02_JS_variables/Ch2_pgm_05.js
+++ 02_JS_variables/Ch2_pgm_05.js
@@ -13,15 +13,24 @@ console.log(message);
 /* Further Adventures
  *
  * 1) Assign message a third value. Display it on the console.
- *
+ */
+message = "This is my third message.";
+console.log(message);
+
+/* 
  * Pieces of text can be joined together using the + symbol.
  * e.g. "Hello " + "World!"
  *
  * 2) Split the first message into two pieces,
  *    joined by + and run the program.
- *
+ */
+message = "Hello " + "World!";
+console.log(message);
+
+/* 
  * 3) Split the second message into three pieces,
  *    joined by + and run the program.
  *    Be careful to use matching speech marks.
- *
  */
+message = 'Congratulations! ' + 'Your tweet ' + 'has won a prize...';
+console.log(message);
