8cde36ce4972642928a2f6893d5311dd7172c2d7
2226j1554 <144467760+2226j1554@users.noreply.github.com>
2024-07-02 09:51:28 +0530

    Update Ch2_pgm_03.js

--- 02_JS_variables/Ch2_pgm_03.js
+++ 02_JS_variables/Ch2_pgm_03.js
@@ -12,9 +12,14 @@ console.log(score);
  *    Run the program again.
  *
  * 2) Declare a second variable, maybe score2.
- *
+ */
+ var score2;
+/*
  * 3) Assign your variable a value.
- *
+ */
+score2 = 75;
+/*
  * 4) Use console.log to display your variable on the console.
  *
  */
+console.log(score2);
