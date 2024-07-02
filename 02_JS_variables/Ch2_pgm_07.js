c8ab50a1212d62a05cea030bce37a2db130522da
2226j1554 <144467760+2226j1554@users.noreply.github.com>
2024-07-02 10:01:25 +0530

    Update Ch2_pgm_07.js

--- 02_JS_variables/Ch2_pgm_07.js
+++ 02_JS_variables/Ch2_pgm_07.js
@@ -11,14 +11,23 @@ console.log(playerName + " is in " + locationName);
  *
  * 1) Declare a health variable and assign it
  *    a value in one step.
- *
+ */
+var health = 50;
+
+/* 
  * 2) Declare a message variable.
- *
+ */
+var message;
+
+/* 
  * 3) Assign message a value created by joining
  *    variables and text to say something like
  *    "Kandra has health 50 and is in The Dungeon of Doom"
- *
+ */
+message = playerName + " has health " + health + " and is in " + locationName;
+
+/* 
  * 4) Replace the current console.log statement
  *    with a new one to display the message variable.
- *
  */
+console.log(message);
