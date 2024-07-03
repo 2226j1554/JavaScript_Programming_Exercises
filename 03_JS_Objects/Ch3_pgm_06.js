5a135c64477e3f89baa5d5b02634110e417a61c9
2226j1554 <144467760+2226j1554@users.noreply.github.com>
2024-07-03 09:38:02 +0530

    Update Ch3_pgm_06.js

--- 03_JS_Objects/Ch3_pgm_06.js
+++ 03_JS_Objects/Ch3_pgm_06.js
@@ -1,28 +1,32 @@
 // Using a variable as a property value
-
 var book;
 var bookName;
 
 bookName = "The Adventures of Tom Sawyer";
 
 book = {
-    title : bookName
+    title: bookName
 };
 
+/* Further Adventures */
 
+// 1) Run the program
 
-/* Further Adventures
- *
- * 1) Run the program.
- *
- * 2) At the console prompt, type 'book'
- *    and press enter.
- *
- * The book object should be displayed on the console.
- * Its title property should have the same
- * value as the bookName variable.
- *
- * 3) Change the value of the bookName variable
- *    and try steps 1 and 2 again.
- *
- */
+// 2) At the console prompt, type 'book' and press enter
+//    This will display the book object with its current title property
+
+// 3) Change the value of the bookName variable and repeat steps 1 and 2
+
+// Initial output
+console.log("Initial book object:");
+console.log(book);
+
+// Change the value of bookName
+bookName = "The Great Gatsby";
+
+// Updated book object with new title
+book.title = bookName;
+
+// Updated output
+console.log("\nUpdated book object:");
+console.log(book);
