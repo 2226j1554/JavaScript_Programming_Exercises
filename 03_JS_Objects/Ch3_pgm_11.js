8eeee401b1f0cfed0325a2c84ed05ad1cc22fa50
2226j1554 <144467760+2226j1554@users.noreply.github.com>
2024-07-03 09:50:42 +0530

    Update Ch3_pgm_11.js

--- 03_JS_Objects/Ch3_pgm_11.js
+++ 03_JS_Objects/Ch3_pgm_11.js
@@ -14,16 +14,16 @@ player1.score = player1.score + 50;
 console.log(player1.name + " has scored " + player1.score);
 
 
+// Increase player1's score by 10%
+player1.score = player1.score * 1.1; // Increases score by 10%
+console.log(player1.name + " now has a score of " + player1.score);
 
-/* Further Adventures
- *
- * 1) Write code to increase player1's score by 10%.
- *
- * 2) Add a second player.
- *
- * 3) Use the players' properties to find the sum
- *    of their scores and log it to the console,
- *    along with an appropriate message that includes
- *    their names.
- *
- */
+// Add a second player
+var player2 = {
+    name: "Alice",
+    score: 0
+};
+
+// Calculate and log the sum of their scores
+var totalScore = player1.score + player2.score;
+console.log(player1.name + " and " + player2.name + " have a total score of " + totalScore);
