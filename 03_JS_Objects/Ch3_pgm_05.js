9253745dd3ddadf3c3d88e958f4caf3521bf935b
2226j1554 <144467760+2226j1554@users.noreply.github.com>
2024-07-03 09:36:04 +0530

    Update Ch3_pgm_05.js

--- 03_JS_Objects/Ch3_pgm_05.js
+++ 03_JS_Objects/Ch3_pgm_05.js
@@ -7,16 +7,23 @@ book = {
 };
 
 
+/* Further Adventures */
 
-/* Further Adventures
- *
- * 1) Log book to the console.
- *
- * 2) Create a second book.
- *
- * 3) Log book2 to the console.
- *
- * 4) Run the program.
+// 1) Log book to the console
+console.log("Book 1:");
+console.log(book);
+
+// 2) Create a second book
+var book2 = {
+    title: "1984",
+    author: "George Orwell"
+};
+
+// 3) Log book2 to the console
+console.log("\nBook 2:");
+console.log(book2);
+
+ /* 4) Run the program.
  *    In the console tab, click after the prompt, >.
  *    Type book and press Enter.
  *    The book object should be displayed.
